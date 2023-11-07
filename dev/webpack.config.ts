21c19ffdf58e058a157a7ea4e42eebac1940824e
Guan <821143943@qq.com>
2023-11-07 13:11:20 +0800

    feat: set up tailwindcss

--- dev/webpack.config.ts
+++ dev/webpack.config.ts
@@ -1,4 +1,8 @@
+import autoprefixer from "autoprefixer"
+import MiniCssExtractPlugin from "mini-css-extract-plugin"
+import tailwindcss from "tailwindcss"
 import path from "path"
+import postcssPresetEnv from "postcss-preset-env"
 import { monkey } from "webpack-monkey"
 
 module.exports = monkey({
@@ -6,12 +10,29 @@ module.exports = monkey({
   output: {
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: /node_modules/,
         use: "ts-loader",
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [postcssPresetEnv(), tailwindcss(), autoprefixer()],
+              },
+            },
+          },
+        ],
       },
     ],
   },
