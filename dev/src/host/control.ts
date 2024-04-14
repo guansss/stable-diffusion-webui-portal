7f24bebdedec367836db38b070b55c9579a99f6b
Guan <821143943@qq.com>
2024-04-15 03:11:25 +0800

    feat: ability to open window in different screens on supported browsers

--- dev/src/host/control.ts
+++ dev/src/host/control.ts
@@ -1,4 +1,5 @@
 import { uniqueId } from "lodash-es"
+import { DEV } from "../constants"
 import { log } from "../utils/log"
 import { getExtensionInfo } from "./remote"
 import { getWindowMetrics } from "./window"
@@ -31,20 +32,30 @@ function insertOpenButton(targetButton: HTMLElement, url: string) {
   openButton.id = tabName + "_open_portal"
 
   openButton.innerText = "🖥️"
-  openButton.title = "Open Portal (stable-diffusion-webui-portal)"
+  openButton.title = "Open Portal (stable-diffusion-webui-portal)" + (DEV ? " (dev)" : "")
   openButton.href = url
   openButton.target = "_blank"
 
-  openButton.addEventListener("click", (e) => {
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  openButton.addEventListener("click", async (e) => {
     e.preventDefault()
     e.stopPropagation()
 
     const { x, y, width, height } = getWindowMetrics()
 
+    try {
+      // this is an experimental browser feature, and is needed for opening the window in different screens
+      if ("getScreenDetails" in unsafeWindow) {
+        await (unsafeWindow as { getScreenDetails: () => Promise<void> }).getScreenDetails()
+      }
+    } catch (e) {
+      log("Failed calling getScreenDetails():", e)
+    }
+
     window.open(
       openButton.href,
       openButton.target,
-      `width=${width},height=${height},screenX=${x},screenY=${y}`,
+      `width=${width}, height=${height}, left=${x}, top=${y}`,
     )
   })
 
